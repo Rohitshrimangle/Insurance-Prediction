435ee2eab24e78b5d192d772de58832922180b08
Rohit Shrimangle <rohitshrimangle431513@gmail.com>
2024-07-23 10:26:13 +0530

    result modified

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ def predict_datapoint():
         
         final_data=data.get_data_as_dataframe()
         
-        predict_pipeline=PredictPipeline(final_data)
+        predict_pipeline=PredictPipeline()
         
-        pred=predict_pipeline.predict()
+        pred=predict_pipeline.predict(final_data)
         
         result=round(pred[0],2)
         
